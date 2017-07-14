e839a7e5e77326831d33f3e8dc4187ebdafc9b33
John Ang <johnang@berkeley.edu>
2017-07-13 23:51:39 -0500

    further refined initial hand point distribution

---
+++ Hand.java
@@ -0,0 +1,57 @@
+
+public class Hand {
+    int initialScore;
+    Card firstCard;
+    Card secondCard;
+
+    /**
+     *
+     * @param firstCard
+     * @param secondCard
+     * @param initialScore
+     * @return A score indicative of how good a player's starting hand is
+     */
+    public int calculateInitialHandScore(Card firstCard, Card secondCard, int initialScore) {
+        int firstCardNumber = firstCard.number;
+        int secondCardNumber = secondCard.number;
+        int firstCardSuit = firstCard.suit;
+        int secondCardSuit = secondCard.suit;
+
+        // Pair, different suits ex. 6 of hearts, 6 of spades
+        if (firstCardNumber == secondCardNumber && firstCardSuit != secondCardSuit) {
+            initialScore*=2;
+        }
+
+        // Same suit ex. 8 of clubs, 2 of clubs
+        if (firstCardSuit == secondCardSuit && firstCardNumber != secondCardNumber) {
+            initialScore = firstCardNumber + secondCardNumber;
+        }
+
+        // High cards Bonus ex. Ace, King, Queen, Jack
+        if (firstCardNumber == 1 || firstCardNumber == 10 || firstCardNumber == 11 || firstCardNumber == 12 || firstCardNumber == 13) {
+            initialScore ++;
+        }
+        if (secondCardNumber == 1 || secondCardNumber == 10 || secondCardNumber == 11 || secondCardNumber == 12 || secondCardNumber == 13) {
+            initialScore ++;
+        }
+        return initialScore;
+    }
+
+    public Hand(Card x, Card y) {
+        firstCard = x;
+        secondCard = y;
+        initialScore = Math.max(firstCard.number, secondCard.number);
+        initialScore = calculateInitialHandScore(firstCard, secondCard, initialScore);
+    }
+
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 20; i++) {
+//            Card x = new Card();
+//            System.out.println("suit: " + x.suit + " number: " + x.number + " cardName: " + x.cardName);
+//            Card y = new Card();
+//            System.out.println("suit: " + y.suit + " number: " + y.number + " cardName: " + y.cardName);
+//            playerHand hand = new playerHand(x, y);
+//            System.out.println("handscore: " + hand.initialScore);
+//        }
+//    }
+}
